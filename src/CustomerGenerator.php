3f539518b564be3c07f444960e5493be65512467
orrbit <orrbit76@gmail.com>
2022-08-18 15:00:29 -0400

    refine the autoload functionality

--- src/CustomerGenerator.php
+++ src/CustomerGenerator.php
@@ -1,18 +1,17 @@
 <?php
-namespace lorr\my_customer_generator;
+    namespace lorr\my_customer_generator;
 
-require __DIR__ . '/../vendor/autoload.php';
-use Ramsey\Uuid\Uuid;
+    require_once "vendor/autoload.php";
+    use Ramsey\Uuid\Uuid;
 
-class CustomerGenerator
-{
-    public static function GenerateCustomer(){
-        $uuid = Uuid::uuid4();
+    class CustomerGenerator
+    {
+        public static function GenerateCustomer(){
+            $uuid = Uuid::uuid4();
 
-        $obj = new \stdClass();
-        $obj->id = $uuid->toString();
-        return $obj;
+            $obj = new \stdClass();
+            $obj->id = $uuid->toString();
+            return $obj;
+        }
     }
-}
-
 ?>
